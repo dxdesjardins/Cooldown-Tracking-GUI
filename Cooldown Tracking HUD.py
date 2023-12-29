75216c1f0ebc49097a00ea9aba75006440528aec
Lambchomp <72995117+dxdesjardins@users.noreply.github.com>
2023-12-29 03:25:24 -0500

    Update Comments

--- Cooldown Tracking HUD.py
+++ Cooldown Tracking HUD.py
@@ -3,10 +3,10 @@ import win32api, win32con, win32gui, math
 import cv2, numpy as np
 import win32ui
 
-# Script that searches for on images at specified screen locations and creates progressbars once the images are found.
+# Script that searches for images at specified screen locations and creates progressbars once the images are found.
 # Configurable to various factors such as: color, size, frequency of trigger, refresh rate, image tolerance.
 # Uses the windows API to draw the progress bar to the screen.
-# Designed to have a bare minimum dependance on external libraries.
+# Designed to have minimumal dependance on high level external libraries.
 #
 # Dependencies: PyWin32, NumPy, OpenCV
 
